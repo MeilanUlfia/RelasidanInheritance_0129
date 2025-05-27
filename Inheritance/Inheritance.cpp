34a19b303b8a3465145d0bec222e01e9fbe318f1
meilan <meilanulfia@gmail.com>
2025-05-27 21:00:49 +0700

    menambah class pelajar turunan dari class Orang dengan atribut sekolah, metode perkenalan.

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -21,3 +21,18 @@ class orang {
             }
 };
 
+class pelajar : public orang {
+    public :
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dohapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
+
